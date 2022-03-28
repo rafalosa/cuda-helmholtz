25df93168870f33a32682a817589483005f7fcac
rafalosa <rafal.osad@gmail.com>
2022-03-28 18:44:20 +0200

    Comments for GPU/CPU compute example.

--- src/examples/gpu_cpu_compute.cu
+++ src/examples/gpu_cpu_compute.cu
@@ -16,7 +16,7 @@ using namespace SimulatorUtils::Structures;
 int main() {
 
     int count;
-    auto err = cudaGetDeviceCount(&count);
+    auto err = cudaGetDeviceCount(&count); // Getting CUDA devices count.
     if(err != cudaSuccess){
 
         std::cout << "Error: " << cudaGetErrorName(err) << std::endl;
@@ -25,41 +25,40 @@ int main() {
 
     std::cout << "Cuda devices available: " << count << std::endl;
 
-    CUDAUtils::showCudaDeviceProps(0);
+    CUDAUtils::showCudaDeviceProps(0); // Displaying properties of 0th device.
 
-    auto ent = std::make_unique<HelmholtzSet>(8, 60, 1.9, 200, SimulatorUtils::Geometry::Plane::XY, 100, 0.1);
+    auto ent = std::make_unique<HelmholtzSet>(8, 60, 1.9, 200, SimulatorUtils::Geometry::Plane::XY, 100, 0.1); // Host memory allocation for HelmholtzSet object.
 
-    auto res = ent->pointInductionVector(make_float3(1.2, 12, 0), 4);
+    auto res = ent->pointInductionVector(make_float3(1.2, 12, 0), 4); // CPU aka Host compute.
 
-    std::cout <<"CPU result: " << res << std::endl;
+    std::cout <<"CPU result: " << res << std::endl;  // Displaying CPU result.
 
-    HelmholtzSet* entOnGPU;
-    float3* resultGPU;
+    HelmholtzSet* entOnGPU; // Host pointer to GPU memory object.
+    float3* resultGPU; // Host pointer to GPU memory storing the result.
 
-    err = cudaMalloc((void**)&resultGPU, sizeof(float3));
+    err = cudaMalloc((void**)&resultGPU, sizeof(float3)); // Allocating GPU memory for result.
+
+    if(err != cudaSuccess){ // "Handling" possible errors.
+
+        std::cout << "Error: " << cudaGetErrorName(err) << std::endl;
+        throw std::runtime_error("Failed to allocate memory on a device.");
+    }
+    err = cudaMalloc((void**)&entOnGPU, sizeof(HelmholtzSet)); // Allocating GPU memory for HelmholtzSet object.
 
     if(err != cudaSuccess){
 
         std::cout << "Error: " << cudaGetErrorName(err) << std::endl;
         throw std::runtime_error("Failed to allocate memory on a device.");
     }
-    err = cudaMalloc((void**)&entOnGPU, sizeof(HelmholtzSet));
 
-    if(err != cudaSuccess){
+    setupGPUHelmholtzEnv<<<1,1>>>(entOnGPU,8,60,1.9,200,SimulatorUtils::Geometry::Plane::XY,100,0.1); // Creating GPU environment for computation aka dynamically allocating the object on the GPU.
 
-        std::cout << "Error: " << cudaGetErrorName(err) << std::endl;
-        throw std::runtime_error("Failed to allocate memory on a device.");
-    }
+    GPUComputeTest<<<1,1>>>(entOnGPU, make_float3(1.2, 12, 0),4,resultGPU); // GPU compute using the object created on the GPU.
 
-    setupGPUHelmholtzEnv<<<1,1>>>(entOnGPU,8,60,1.9,200,SimulatorUtils::Geometry::Plane::XY,100,0.1);
+    shutdownGPUHelmholtzEnv<<<1,1>>>(entOnGPU); // Shutting down the GPU environment aka deallocating the created object.
 
-    GPUComputeTest<<<1,1>>>(entOnGPU, make_float3(1.2, 12, 0),4,resultGPU);
-
-    shutdownGPUHelmholtzEnv<<<1,1>>>(entOnGPU);
-
-    //float3 resultHost;
-    auto resultHost = std::make_unique<float3>();
-    err = cudaMemcpy(resultHost.get(), resultGPU, sizeof(float3), cudaMemcpyDeviceToHost);
+    auto resultHost = std::make_unique<float3>(); // Pointer to GPU result on Host.
+    err = cudaMemcpy(resultHost.get(), resultGPU, sizeof(float3), cudaMemcpyDeviceToHost); // Copying GPU result memory to Host pointer.
 
     if(err != cudaSuccess){
 
@@ -67,10 +66,10 @@ int main() {
         throw std::runtime_error("Failed to memcpy from device.");
     }
 
-    cudaFree(resultGPU);
+    cudaFree(resultGPU); // Deallocating memory on GPU.
     cudaFree(entOnGPU);
 
-    std::cout <<"GPU result: " << *resultHost << std::endl;
+    std::cout <<"GPU result: " << *resultHost << std::endl; // Displaying GPU result.
 
     return 0;
 }
