76332d7216e3405c532e396323dc7fc0403f0302
rafalosa <rafal.osad@gmail.com>
2022-04-16 22:25:23 +0200

    New target

--- src/examples/mesh_eval_test.cu
+++ src/examples/mesh_eval_test.cu
@@ -1,31 +1,27 @@
 #include <iostream>
 #include <memory>
-
 #include "cuda_runtime_api.h"
 #include "cuda_device_runtime_api.h"
-#include "HelmholtzEnvKernels.cuh"
-#include "HelmholtzSet.cuh"
-#include "Utils.cuh"
 #include "Mesher.cuh"
 #include "cuda_float3_operators.cuh"
 #include "vector_types.h"
 #include "CudaMacros.cuh"
-#include "CudaAllocatorInterfaces.cuh"
+#include "HelmholtzSet.cuh"
+#include "Utils.cuh"
 
 using namespace CUDAUtils::Memory;
 
 int main() {
 
-//    auto GPUCoils = newCudaInstance<HelmholtzSet>(8, 60, 1.9, 200,
-//                                                                     SimulatorUtils::Geometry::Plane::XY, 100, 0.1);
-//    auto GPUMesh = newCudaInstance<Mesh<MeshUtils::Dim::D3, 10>>
-//    (MeshUtils::Units::CENTIMETERS, -100, 100, -100, 100, -100, 100);
-//
-//    /// Calculations code....
-//
-//    deleteCudaInstance(GPUMesh);
-//    deleteCudaInstance(GPUCoils);
+    auto GPUCoils = newCudaInstance<HelmholtzSet>(8, .5, .0019, .74,
+                                                                     SimulatorUtils::Geometry::Plane::XY, 100, 0.1);
+    auto GPUMesh = newCudaInstance<Mesh<MeshUtils::Dim::D3, 10>>
+    (MeshUtils::Units::CENTIMETERS, -100, 100, -100, 100, -100, 100);
 
+    /// Calculations code....
+
+    deleteCudaInstance(GPUMesh);
+    deleteCudaInstance(GPUCoils);
 
     return 0;
 }
